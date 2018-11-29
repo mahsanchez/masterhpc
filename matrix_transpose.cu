0cf6f4c5e489de747bcbff013ede0f1e506e0396
mahsanchez <mahsanchez@users.noreply.github.com>
2018-11-29 21:26:34 +0100

    Assignment 5

--- matrix_transpose.cu
+++ matrix_transpose.cu
@@ -1,127 +1,77 @@
 #include <stdio.h>
-#include <assert.h>
-
-#include <cuda_runtime_api.h>
-#include <driver_types.h>
-#include <cuda.h>
 
 const int TILE_DIM = 32;
-const int BLOCK_ROWS = 8;
-const int NUM_REPS = 100;
 
-inline cudaError_t checkCuda(cudaError_t result)
-{
+inline cudaError_t checkCuda(cudaError_t result) {
   if (result != cudaSuccess) {
-    fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
-    assert(result == cudaSuccess);
-  }
+    printf("CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+	exit(1);
+  }	
   return result;
 }
 
-void postprocess(const float *ref, const float *res, int n, float ms)
-{
-  bool passed = true;
-  for (int i = 0; i < n; i++)
-    if (res[i] != ref[i]) {
-      printf("%d %f %f\n", i, res[i], ref[i]);
-      printf("%25s\n", "*** FAILED ***");
-      passed = false;
-      break;
-    }
-  if (passed)
-    printf("%20.2f\n", 2 * n * sizeof(float) * 1e-6 * NUM_REPS / ms );
-}
 
-
-__global__ void imatcopy(float *odata, const float *idata)
+__global__ void transposeMatrix(float *B, float *A, int width)
 {
   __shared__ float tile[TILE_DIM][TILE_DIM];
-    
   int x = blockIdx.x * TILE_DIM + threadIdx.x;
   int y = blockIdx.y * TILE_DIM + threadIdx.y;
-  int width = gridDim.x * TILE_DIM;
-
-  for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
-     tile[threadIdx.y+j][threadIdx.x] = idata[(y+j)*width + x];
-     //tile[(threadIdx.y+j)*TILE_DIM + threadIdx.x] = idata[(y+j)*width + x];
-
-  __syncthreads();
-
-  x = blockIdx.y * TILE_DIM + threadIdx.x;  // transpose block offset
-  y = blockIdx.x * TILE_DIM + threadIdx.y;
-
-  for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
-     odata[(y+j)*width + x] = tile[threadIdx.x][threadIdx.y + j];
-}
   
+  tile[threadIdx.y][threadIdx.x] = A[y*width + x];
+  __syncthreads();
+  
+  x = blockIdx.y * TILE_DIM + threadIdx.x;  
+  y = blockIdx.x * TILE_DIM + threadIdx.y;
+  
+  B[y*width + x] = tile[threadIdx.x][threadIdx.y];
+}
+
+
 int main(int argc, char **argv)
 {
-  const int nx = 1024;
-  const int ny = 1024;
-  const int mem_size = nx*ny*sizeof(float);
+  // Number of rows and columns must be a multiple of 32
+  const int rows = 2048;
+  const int columns = 2048;
+  const int width = rows;
+  const int size = rows*columns*sizeof(float);
 
-  dim3 dimGrid(nx/TILE_DIM, ny/TILE_DIM, 1);
-  dim3 dimBlock(TILE_DIM, BLOCK_ROWS, 1);
+  dim3 dimGrid(width/TILE_DIM, width/TILE_DIM);
+  dim3 dimBlock(TILE_DIM, TILE_DIM);
 
-  float *h_idata = (float*)malloc(mem_size);
-  float *h_tdata = (float*)malloc(mem_size);
-  float *gold    = (float*)malloc(mem_size);
+  float *A = (float*)malloc(size);
+  float *B = (float*)malloc(size);
+  float *C = (float*)malloc(size);
   
-  float *d_idata, *d_tdata;
-  checkCuda( cudaMalloc(&d_idata, mem_size) );
-  checkCuda( cudaMalloc(&d_tdata, mem_size) );
-
-  // check parameters and calculate execution configuration
-  if (nx % TILE_DIM || ny % TILE_DIM) {
-    printf("nx and ny must be a multiple of TILE_DIM\n");
-    goto error_exit;
-  }
-
-  if (TILE_DIM % BLOCK_ROWS) {
-    printf("TILE_DIM must be a multiple of BLOCK_ROWS\n");
-    goto error_exit;
-  }
+  float *dA, *dB;
+  checkCuda( cudaMalloc(&dA, size) );
+  checkCuda( cudaMalloc(&dB, size) );
     
-  // host
-  for (int j = 0; j < ny; j++)
-    for (int i = 0; i < nx; i++)
-      h_idata[j*nx + i] = j*nx + i;
+  for (int j = 0; j < rows; j++)
+    for (int i = 0; i < columns; i++)
+      A[j*width + i] = 0.15*i + 0.1*j;
   
-  // correct result for error checking
-  for (int j = 0; j < ny; j++)
-    for (int i = 0; i < nx; i++)
-      gold[j*nx + i] = h_idata[i*nx + j];
-  
-  // device
-  checkCuda( cudaMemcpy(d_idata, h_idata, mem_size, cudaMemcpyHostToDevice) );
-  
-  // events for timing
-  cudaEvent_t startEvent, stopEvent;
-  checkCuda( cudaEventCreate(&startEvent) );
-  checkCuda( cudaEventCreate(&stopEvent) );
-  float ms;
+  for (int j = 0; j < rows; j++)
+    for (int i = 0; i < columns; i++)
+      C[j*width + i] = A[i*width + j];
 
-  printf("%25s", "coalesced transpose");
-  checkCuda( cudaMemset(d_tdata, 0, mem_size) );
-  // warmup
-  imatcopy<<<dimGrid, dimBlock>>>(d_tdata, d_idata);
-  checkCuda( cudaEventRecord(startEvent, 0) );
+  checkCuda( cudaMemcpy(dA, A, size, cudaMemcpyHostToDevice) );
   
-  for (int i = 0; i < NUM_REPS; i++)
-     imatcopy<<<dimGrid, dimBlock>>>(d_tdata, d_idata);
- 
-  checkCuda( cudaEventRecord(stopEvent, 0) );
-  checkCuda( cudaEventSynchronize(stopEvent) );
-  checkCuda( cudaEventElapsedTime(&ms, startEvent, stopEvent) );
-  checkCuda( cudaMemcpy(h_tdata, d_tdata, mem_size, cudaMemcpyDeviceToHost) );
-  postprocess(gold, h_tdata, nx * ny, ms);
+  transposeMatrix<<<dimGrid, dimBlock>>>(dB, dA, width);
+  
+  checkCuda( cudaDeviceSynchronize() );
+  
+  checkCuda( cudaMemcpy(B, dB, size, cudaMemcpyDeviceToHost) );
+  
+	for (int i = 0; i < rows * columns; i++)
+	{
+		if (B[i] != C[i]) {
+		  printf("%d %f %f INVALID RESULTS \n", i, B[i], C[i]);
+		}
+	}	
 
-error_exit:
-  checkCuda( cudaEventDestroy(startEvent) );
-  checkCuda( cudaEventDestroy(stopEvent) );
-  checkCuda( cudaFree(d_tdata) );
-  checkCuda( cudaFree(d_idata) );
-  free(h_idata);
-  free(h_tdata);
-  free(gold);
+  checkCuda( cudaFree(dA) );
+  checkCuda( cudaFree(dB) );
+  free(A);
+  free(B);
+  free(C);
 }
