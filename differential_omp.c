76cf90fe71d76bb14c5f0f34651ff2851a175451
mahsanchez <mahsanchez@users.noreply.github.com>
2018-12-09 17:54:47 +0100

    Add files via upload
    OpenMP-Final_Poject

--- differential_omp.c
+++ differential_omp.c
@@ -14,6 +14,8 @@ double uexact[NX][NY];
 double unew[NX][NY];
 double f[NX][NY];
 
+double v;
+
 int main ( int argc, char *argv[] );
 double norm ( int nx, int ny, double a[NX][NY] );
 void initial ( int nx, int ny, double f[NX][NY] );
@@ -25,7 +27,7 @@ double evaldxdyexact ( double x, double y );
 
 int main ( int argc, char *argv[] )
 {
-  int converged;
+  int converged = 0;
   double diff;
   double dx;
   double dy;
@@ -62,15 +64,19 @@ int main ( int argc, char *argv[] )
     printf ( "  Y grid spacing: %f\n", dy );
 	
 clock_t tStart = clock();
-/*
-  Initialize the data
-*/
-  initial ( nx, ny, f );
+
 /*
   Set the initial solution
 */
  #pragma omp parallel  
  {
+	  /*
+         Initialize the data
+      */
+      initial ( nx, ny, f );
+      /*
+         Set the initial solution
+      */
 	  #pragma omp for private(i, j)
 	  for ( i = 0; i < nx; i++ )
 	  {
@@ -85,15 +91,13 @@ clock_t tStart = clock();
 			unew[i][j] = 0.0;
 		  }
 		}
-	  }	  
-  }
-  unew_norm = norm ( nx, ny, unew );
-/*
-  Set the exact solution.
-*/
-  #pragma omp parallel 
-  {
-	  #pragma omp for private(i, j)
+	  }	  	  
+	  
+      unew_norm = norm ( nx, ny, unew );	  
+	  /*
+      Set the exact solution.
+      */	  
+	  #pragma omp for private(i, j, x, y)
 	  for ( i = 0; i < nx; i++ )
 	  {
 		x = ( double ) ( i ) / ( double ) ( nx - 1 );
@@ -103,35 +107,37 @@ clock_t tStart = clock();
 		  uexact[i][j] = evalexact ( x, y );
 		}
 	  }
-  }
-  u_norm = norm ( nx, ny, uexact );
-  printf ( "  Norm of exact solution = %g\n", u_norm );
-/*
-  Iteration
-*/
-  converged = 0;
+	  
+      u_norm = norm ( nx, ny, uexact );
+	  
+	  #pragma omp single
+      printf ( "  Norm of exact solution = %g\n", u_norm );	  
+	  /*
+       Iteration
+      */	  
+	  #pragma omp single
+          printf ( " \n Step    ||Unew||     ||Diff||     ||Error||\n\n" );
 
-  printf ( "\n" );
-  printf ( "  Step    ||Unew||     ||Diff||     ||Error||\n" );
-  printf ( "\n" );
-
-  #pragma omp parallel
-  {
-	  #pragma omp for private(i, j)
+      #pragma omp for private(i, j)
 	  for ( i = 0; i < nx; i++ )
 	  {
 		for ( j = 0; j < ny; j++ )
 		{
 		  udiff[i][j] = unew[i][j] - uexact[i][j];
 		}
-	  }
-  }
-  error = norm ( nx, ny, udiff );
-  printf ( "  %4d  %14g                  %14g\n", 0, unew_norm, error );
+	  }	 
 
-  for ( it = 1; it <= it_max; it++ )
+	 error = norm ( nx, ny, udiff );
+	 
+	 #pragma omp single
+	 printf ( "  %4d  %14g  %14g\n", 0, unew_norm, error );
+	 	
+  }
+  
+  
+  for ( it = 1; it <= it_max; it++ ) 
   {
-	#pragma omp parallel
+	#pragma omp parallel    
 	{	
 	    #pragma omp for private(i, j)
 		for ( i = 0; i < nx; i++ )
@@ -141,16 +147,16 @@ clock_t tStart = clock();
 			u[i][j] = unew[i][j];
 		  }
 		}
-	}
-    iteration ( nx, ny, dx, dy, f, u, unew );
-/*
-  Check for convergence.
-*/
-    u_norm = unew_norm;
-    unew_norm = norm ( nx, ny, unew );
+		
+		iteration ( nx, ny, dx, dy, f, u, unew );		
+		/*
+         Check for convergence.
+        */ 
+        #pragma omp single		
+		u_norm = unew_norm; 
+		    
+        unew_norm = norm ( nx, ny, unew );
 
-	#pragma omp parallel
-	{
 		#pragma omp for private(i, j)
 		for ( i = 0; i < nx; i++ )
 		{
@@ -158,33 +164,31 @@ clock_t tStart = clock();
 		  {
 			udiff[i][j] = unew[i][j] - u[i][j];
 		  }
-		}
-	}
-    diff = norm ( nx, ny, udiff );
-
-	#pragma omp parallel
-	{
+		}	
+		 
+        diff = norm ( nx, ny, udiff );
+		
 		#pragma omp for private(i, j)
 		for ( i = 0; i < nx; i++ )
-		{
+		{	
 		  for ( j = 0; j < ny; j++ )
 		  {
 			udiff[i][j] = unew[i][j] - uexact[i][j];
 		  }
 		}
+		
+		error = norm ( nx, ny, udiff );
+		
+		#pragma omp single
+		printf ( "  %4d  %14g  %14g  %14g\n", it, unew_norm, diff, error );
 	}
-    error = norm ( nx, ny, udiff );
 
-    printf ( "  %4d  %14g  %14g  %14g\n", it, unew_norm, diff, error );
-
-    if ( diff <= tolerance )
-    {
-      converged = 1;
-      break;
-    }
-
+	if ( diff <= tolerance )
+	{
+	  converged = 1;
+	  break;
+	}
   }
-  
 
   if ( converged )
   {
@@ -212,27 +216,25 @@ double norm ( int nx, int ny, double a[NX][NY] )
 {
   int i;
   int j;
-  double v;
 
   v = 0.0;
-  
-  #pragma omp parallel  
+
+  #pragma omp for private(i, j) reduction(+:v)
+  for ( i = 0; i < nx; i++ )
   {
-	  #pragma omp for private(i, j) reduction(+:v)
-	  for ( i = 0; i < nx; i++ )
-	  {
-		#pragma omp simd  
-		for ( j = 0; j < ny; j++ )
-		{
-		  v = v + a[i][j] * a[i][j];
-		}
-	  }
+    for ( j = 0; j < ny; j++ )
+    {	
+      v = v + a[i][j] * a[i][j];
+    }
   }
   
+  #pragma omp single
   v = sqrt ( v / ( double ) ( nx * ny )  );
 
   return v;
 }
+
+
 /******************************************************************************/
 
 void initial ( int nx, int ny, double f[NX][NY] )
@@ -243,31 +245,30 @@ void initial ( int nx, int ny, double f[NX][NY] )
   double x;
   double y;
 
-  #pragma omp parallel  
-  {  
-      #pragma omp for private(i, j, x, y)
-	  for ( i = 0; i < nx; i++ )
+  #pragma omp for private(i, j, x, y)
+  for ( i = 0; i < nx; i++ )
+  {
+	x = ( double ) ( i ) / ( double ) ( nx - 1 );
+	
+	for ( j = 0; j < ny; j++ )
+	{
+	  y = ( double ) ( j ) / ( double ) ( ny - 1 );
+	  if ( i == 0 || i == nx - 1 || j == 0 || j == ny - 1 )
 	  {
-		x = ( double ) ( i ) / ( double ) ( nx - 1 );
-		#pragma omp simd
-		for ( j = 0; j < ny; j++ )
-		{
-		  y = ( double ) ( j ) / ( double ) ( ny - 1 );
-		  if ( i == 0 || i == nx - 1 || j == 0 || j == ny - 1 )
-		  {
-			f[i][j] = evalexact ( x, y );
-		  }
-		  else
-		  {
-			f[i][j] = - evaldxdyexact ( x, y );
-		  }
-		}
+		f[i][j] = evalexact ( x, y );
 	  }
+	  else
+	  {
+		f[i][j] = - evaldxdyexact ( x, y );
+	  }
+	}
   }
-
+  
   fnorm = norm ( nx, ny, f );
-
-  printf ( "  Norm of F = %g\n", fnorm );
+  
+  if (omp_get_thread_num() == 0) {
+	  printf ( "  Norm of F = %g\n", fnorm );
+  }
 
   return;
 }
@@ -277,26 +278,23 @@ void iteration ( int nx, int ny, double dx, double dy, double f[NX][NY], double 
   int i;
   int j;
 
-  #pragma omp parallel  
+  #pragma omp for private(i, j)
+  for ( i = 0; i < nx; i++ )
   {
-	  #pragma omp for private(i, j)
-	  for ( i = 0; i < nx; i++ )
+	for ( j = 0; j < nx; j++ )
+	{
+	  if ( i == 0 || j == 0 || i == nx - 1 || j == ny - 1 )
 	  {
-		#pragma omp simd  
-		for ( j = 0; j < nx; j++ )
-		{
-		  if ( i == 0 || j == 0 || i == nx - 1 || j == ny - 1 )
-		  {
-			unew[i][j] = f[i][j];
-		  }
-		  else
-		  { 
-			unew[i][j] = 0.25 * ( 
-			  u[i-1][j] + u[i][j+1] + u[i][j-1] + u[i+1][j] + f[i][j] * dx * dy );
-		  }
-		}
+		unew[i][j] = f[i][j];
 	  }
+	  else
+	  { 
+		unew[i][j] = 0.25 * ( 
+		  u[i-1][j] + u[i][j+1] + u[i][j-1] + u[i+1][j] + f[i][j] * dx * dy );
+	  }
+	}
   }
+	  
   return;
 }
 
